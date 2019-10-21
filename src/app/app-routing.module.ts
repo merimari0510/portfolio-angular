cf185bd69b165c1c14a28e5c95991f9035dba6ef
meri051094 <merimari051094@gmail.com>
2019-10-21 11:41:43 +0200

    Add project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+
+import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { ItemComponent } from './pages/item/item.component';
+import { SearchComponent } from './pages/search/search.component';
+
+
+const app_routes: Routes = [
+    { path: 'home', component: PortafolioComponent },
+    { path: 'item/:id', component: ItemComponent },
+    { path: 'search/:termino', component: SearchComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot( app_routes, { useHash: true } )
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule { }
+
+
+
+
+
+
